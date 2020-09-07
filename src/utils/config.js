5cb3aaf3a36a37f42b908a33d72a7b767c773653
Prakash Ranjan <51481476+introidx@users.noreply.github.com>
2020-09-07 23:18:06 +0530

    Add files via upload

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+//TODO: add firebase configuration and export it
+// Your web app's Firebase configuration
+
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
